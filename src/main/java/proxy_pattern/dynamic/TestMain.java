b44ad57b3afb25e9504f0c494e68bc6a6267f1b2
魏俊峰 <2474530605@qq.com>
2020-06-04 18:06:41 +0800

    设计模式培训内容

--- src/main/java/proxy_pattern/dynamic/TestMain.java
+++ src/main/java/proxy_pattern/dynamic/TestMain.java
@@ -8,7 +8,9 @@ import java.lang.reflect.Proxy;
 
 public class TestMain {
     public static void main(String[] args) {
+        //被代理的目标对象
         IOrderService target = new OrderServiceImpl();
+        //动态代理的handler
         InvocationHandler handler = new ProxyHandler(target);
         IOrderService orderService = (IOrderService) Proxy.newProxyInstance(target.getClass().getClassLoader(),
                 target.getClass().getInterfaces(),handler);
--- src/main/java/singleton/SingleTon1.java
+++ src/main/java/singleton/SingleTon1.java
@@ -8,6 +8,7 @@ public class SingleTon1 {
     //私有化构造器
     private SingleTon1() {
     }
+    //加载->验证->准备->解析->初始化
     //类加载机制中：进行初始化时创建为此类变量进行赋值
     private static final SingleTon1 singleTon1 = new SingleTon1();
 
